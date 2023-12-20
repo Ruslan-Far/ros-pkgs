a6cf955556f0e27a2f91b006db20c1772fcb9aba
Ruslan-Far <rfarhetdinov@mail.ru>
2023-12-21 01:00:05 +0300

    removed unnecessary in server

--- semester_work/src/node_server.cpp
+++ semester_work/src/node_server.cpp
@@ -2,7 +2,6 @@
 #include "nav_msgs/OccupancyGrid.h"
 #include "semester_work/CleanedMap.h"
 #include <vector>
-#include <stdio.h>
 
 using namespace std;
 
@@ -203,67 +202,6 @@ int main(int argc, char **argv)
 	ros::ServiceServer server = nh.advertiseService("cleaned_map", cleanMap);
 
 	ROS_INFO("SERVER");
-
-	// grid.resize(6);
-	// for (int i = 0; i < grid.size(); i++)
-	// {
-	// 	grid[i].resize(6);
-	// }
-	// grid[1][1] = true;
-	// grid[0][1] = true;
-	// grid[1][2] = true;
-	// grid[0][3] = true;
-	// grid[1][3] = true;
-	// grid[2][3] = true;
-	// grid[0][4] = true;
-	// grid[2][4] = true;
-	// grid[0][5] = true;
-	// grid[1][5] = true;
-	// grid[2][5] = true;
-
-
-	// grid[2][1] = true;
-	// grid[1][2] = true;
-	// grid[2][2] = true;
-	// grid[3][3] = true;
-	// grid[4][4] = true;
-	// grid[5][5] = true;
-	// grid[4][3] = true;
-	// grid[5][3] = true;
-	// grid[4][4] = true;
-	// grid[4][5] = true;
-	// grid[5][4] = true;
-	// grid[3][4] = true;
-	// grid[2][4] = true;
-
-	// for (int i = 0; i < grid.size(); i++)
-	// {
-	// 	for (int j = 0; j < grid[i].size(); j++)
-	// 	{
-	// 		printf("%d ", grid[i][j] ? 1 : 0);
-	// 	}
-	// 	printf("\n");
-	// }
-	
-	
-	
-	
-	
-	// clean();
-	
-	
-	
-	
-
-	// printf("------------------------------------------------------------------\n");
-	// for (int i = 0; i < grid.size(); i++)
-	// {
-	// 	for (int j = 0; j < grid[i].size(); j++)
-	// 	{
-	// 		printf("%d ", grid[i][j] ? 1 : 0);
-	// 	}
-	// 	printf("\n");
-	// }
 	ros::spin();
 	return 0;
 }
